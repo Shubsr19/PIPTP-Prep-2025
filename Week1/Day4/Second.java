44828350f67ddc8cebf8f6ec25f714d42cec149a
Shubh <shubh.srivastav_cs22@gla.ac.in>
2025-07-31 14:16:31 +0530

    Initial commit

---
+++ Week1/Day4/Second.java
@@ -0,0 +1,11 @@
+public class Second {
+    public static void main(String[] args) {
+        int x = 5;
+
+        if (x > 0 && x % 2 == 1) {
+            System.out.println("Positive odd");
+        } else {
+            System.out.println("Other");
+        }
+    }
+}
