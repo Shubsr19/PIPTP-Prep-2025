fb17b1bed7ba72c4772678e7c96a028a557cc267
Shubh <shubh.srivastav_cs22@gla.ac.in>
2025-08-01 14:48:24 +0530

    Initial Commit

---
+++ Week1/Day5/Question20.java
@@ -0,0 +1,10 @@
+public class Question20 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        if ((a > 3 && b < 3) || (a - b > 1))
+            System.out.println("Q20 Output: True");
+        else
+            System.out.println("Q20 Output: False");
+    }
+}
---
+++ Week1/Day5/Question23.java
@@ -0,0 +1,7 @@
+public class Question23 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = (a << 2) | 1;
+        System.out.println("Q23 Output: " + b); // 9
+    }
+}
