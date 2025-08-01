3938c17ce949092d8527f9edd5d05f49a6575dbd
Shubh <shubh.srivastav_cs22@gla.ac.in>
2025-08-01 14:40:56 +0530

    Initial Commit

---
+++ Week1/Day5/Question21.java
@@ -0,0 +1,8 @@
+public class Question21 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int c = (a & b) << 1;
+        System.out.println("Q21 Output: " + c); // 2
+    }
+}
---
+++ Week1/Day5/Question22.java
@@ -0,0 +1,8 @@
+public class Question22 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int c = a ^ (b & a);
+        System.out.println("Q22 Output: " + c); // 4
+    }
+}
