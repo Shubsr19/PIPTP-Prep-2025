d62077fb2074bf855ef6ffde297c1b630ada57d5
Shubh <shubh.srivastav_cs22@gla.ac.in>
2025-08-01 13:35:15 +0530

    Initial Commit

---
+++ Week1/Day5/Question11.java
@@ -0,0 +1,8 @@
+public class Question11 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+        int z = x + y * x - 2;
+        System.out.println("Q11 Output: " + z); 
+    }
+}
