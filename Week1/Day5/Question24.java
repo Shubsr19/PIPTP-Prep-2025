c74180c6f5b834d44905ae3ca53d2a45a3e98b2c
Shubh <shubh.srivastav_cs22@gla.ac.in>
2025-08-01 14:54:09 +0530

    Initial Commit

---
+++ Week1/Day5/Question24.java
@@ -0,0 +1,9 @@
+public class Question24 {
+    public static void main(String[] args) {
+        int a = 16;
+        int b = a >> (2 + 1);
+        System.out.println("Q24 Output: " + b); // 2
+    }
+}
+
+
