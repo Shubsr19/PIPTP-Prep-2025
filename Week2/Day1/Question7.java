417dded9cd341ef26d612788e9b8511a2a01a078
Shubh <shubh.srivastav_cs22@gla.ac.in>
2025-08-04 22:19:22 +0530

    Initial Commit

---
+++ Week2/Day1/Question7.java
@@ -0,0 +1,11 @@
+public class Question7 {
+    public static int fun(int n) {
+        if (n <= 1)
+            return n;
+        return fun(n - 1) + fun(n - 3);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fun(5)); // Output: 1
+    }
+}
