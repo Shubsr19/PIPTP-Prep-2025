7814304566967a342faa5852f38fa33218e6c11b
Shubh <shubh.srivastav_cs22@gla.ac.in>
2025-08-04 14:45:29 +0530

    Initial Commit

---
+++ Week2/Day1/Question5.java
@@ -0,0 +1,11 @@
+public class Question5 {
+    public static int fact(int n) {
+        if (n == 0)
+            return 1;
+        return n * fact(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fact(4)); // Output: 24
+    }
+}
