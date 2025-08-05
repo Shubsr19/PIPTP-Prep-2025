eaff27c214565c324d1e03060d611bf5adf95a7e
Shubh <shubh.srivastav_cs22@gla.ac.in>
2025-08-05 14:18:01 +0530

    Initial Commit

---
+++ Week2/Day2/Question22.java
@@ -0,0 +1,11 @@
+public class Question22 {
+    public static void update(int[] arr, int i) {
+        arr[i] = arr[i] + 5;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        update(arr, 1);
+        System.out.println(arr[1]);
+    }
+}
---
+++ Week2/Day2/Question23.java
@@ -0,0 +1,13 @@
+public class Question23 {
+    static int x = 100;
+
+    public static void check() {
+        int x = 50; 
+        System.out.println(x); 
+    }
+
+    public static void main(String[] args) {
+        check();
+        System.out.println(x); 
+    }
+}
