a7dfefe7bdabc30dc6e15e1083509c711167a342
Shubh <shubh.srivastav_cs22@gla.ac.in>
2025-08-06 14:41:44 +0530

    Initial Commit

---
+++ Week2/Day3/Question43.java
@@ -0,0 +1,10 @@
+public class Question43 { 
+    public static int process(int x) { 
+    return x * 2; 
+ } 
+    public static void main(String[] args) { 
+        int x = 3; 
+        int y = process(process(x)); 
+        System.out.println(y); //prints 12
+ } 
+} 
---
+++ Week2/Day3/Question44.java
@@ -0,0 +1,9 @@
+public class Question44 { 
+    public static int call(int x) { 
+    return x + 2; 
+ } 
+    public static void main(String[] args) { 
+    int result = call(call(call(0))); 
+    System.out.println(result); //prints 6
+ } 
+} 
